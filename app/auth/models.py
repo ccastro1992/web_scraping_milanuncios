43bc8c9800ca524568645502874fd00f942ec20e
crisc1992 <cristhianc10@hotmail.com>
2021-10-15 23:59:10 -0500

    Agrego funcionalidad de autenticacion de usuarios

---
+++ app/auth/models.py
@@ -0,0 +1,24 @@
+from flask_login import UserMixin
+from app.db import db, BaseModelMixin
+
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+class User(db.Model, BaseModelMixin, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(256), unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+    @staticmethod
+    def get_by_email(email):
+        return User.query.filter_by(email=email).first()
+
+    def __repr__(self):
+        return '<User {}>'.format(self.email)
